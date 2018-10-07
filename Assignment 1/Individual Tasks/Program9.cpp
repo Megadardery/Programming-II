ba4d06fab45ba8d30be80b3dc03af9006bd4b597
Megadardery <Megadardery@yahoo.com>
2018-10-07 16:29:33 +0200

    header and ocd

--- Assignment 1/Individual Tasks/Program9.cpp
+++ Assignment 1/Individual Tasks/Program9.cpp
@@ -1,3 +1,13 @@
+// Course:  CS213 - Programming II  - 2018
+// Title:   Assignment I - Task 2 - Problem 9
+// Program: Grade Count
+// Author:  Adham Mamdouh Mohammed
+// Date:    7 October 2018
+// Version: 1.0
+// Student Name : Adham Mamdouh Mohammed
+// Student ID : 20170039
+// Student Group : G_2
+
 #include <iostream>
 #include <vector>
 
@@ -17,17 +27,17 @@ int main()
     int* a = new int[mx+5];
     int sz = v.size();
     
-    for(int i = 0 ; i <= mx ; ++i){
+    for(int i = 0 ; i <= mx ; ++i)
         a[i] = 0;
-    }
     
-    for(int i = 0 ; i < sz ; ++i){
+    
+    for(int i = 0 ; i < sz ; ++i)
         a[v[i]]++;
-    }
+    
     
     for(int i = 0 ; i <= mx ; ++i){
         if(a[i])
-        cout << "Number of " << i << "'s:    " << a[i] << endl;
+            cout << "Number of " << i << "'s:    " << a[i] << endl;
     }
 
     return 0;

9d5b1aa37427cae68128a7e5df1c931edaf79528
Belal Hamdy <36121094+belalhamdy@users.noreply.github.com>
2018-10-27 23:36:21 +0200

    Create main.cpp

---
+++ Assignment 2/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "stack.h"
+using namespace std;
+
+int main()
+{
+    Stack s(5);
+    s.print();
+    s.push(6);
+    s.print();
+    s.pop();
+    s.print();
+    s.pop();
+    s.print();
+    cout << s.isEmpty() << "\n";
+    s.clear();
+    cout << s.isEmpty() << "\n";
+    s.print();
+    cout << "\ndone\n";
+    int n ;
+    cin >> n;
+    return 0;
+}

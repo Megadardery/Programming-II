a3e186411529064da70912df5c3c1b8a6737e600
Belal Hamdy <36121094+belalhamdy@users.noreply.github.com>
2018-11-17 21:52:08 +0200

    added comments to elaborate.

--- Assignment 3/Task 1/src/Food.cpp
+++ Assignment 3/Task 1/src/Food.cpp
@@ -1,10 +1,11 @@
 #include "Food.h"
 #define max(a,b) ((a)>(b)? (a) : (b))
-
+// default constructor for the food that makes the quantity and price and name is nothing
 Food::Food() {
 	qty = price = 0;
 	name = "";
 }
+// constructor that builds the item and making sure that there's no negative inputs
 Food::Food(std::string n, int p, int q) {
 	qty = max(q, 0);
 	price = max(p, 0);
@@ -13,12 +14,12 @@ Food::Food(std::string n, int p, int q) {
 
 Food::Food(std::string n, double priceInPounds, int q)
 {
-	int priceInPiastres = priceInPounds * 100;
-	priceInPiastres = (priceInPiastres + 24) / 25;
-	priceInPiastres *= 25;
+	int priceInPiasters = priceInPounds * 100; //converts the pounds to piasters
+	priceInPiasters = (priceInPiasters + 24) / 25;
+	priceInPiasters *= 25; // gets the ceil of the price in piasters to prevent truncation errors
 
-	qty = max(q, 0);
-	price = max(priceInPiastres, 0);
+	qty = max(q, 0); // making sure that the quantity is positive
+	price = max(priceInPiasters, 0); // making sure that the price is positive
 	name = n;
 }
 
@@ -33,7 +34,7 @@ std::string Food::getName() const {
 int Food::getQuantity() const {
 	return qty;
 }
-
+// taking one item from the warehouse but making sure first that there is available items to take from
 void Food::takeOne() {
 	if (qty > 0)
 		qty--;

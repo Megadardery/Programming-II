5900e8a1719b0450b8924b35477cfadbbedc9c49
Belal Hamdy <36121094+belalhamdy@users.noreply.github.com>
2018-11-17 07:47:08 +0200

    Added the coin change function .. not tested

--- Assignment 3/Task 1/src/VendingMachine.cpp
+++ Assignment 3/Task 1/src/VendingMachine.cpp
@@ -1,5 +1,5 @@
 #include "VendingMachine.h"
-
+#include<iostream>
 VendingMachine::VendingMachine()
 {
 
@@ -8,13 +8,52 @@ VendingMachine::VendingMachine(Food* items, const Money& initialCoins)
 {
 	for (int i = 0; i < MAX_ITEMS; ++i)
 		this->items[i] = items[i];
-	
+
 	coins = initialCoins;
 }
 bool VendingMachine::convertMoney(int curr, Money& ans) const
 {
+    int tmpcurr;
+    bool flag = true ,startflag = false;
+    for (int start = 0 ; flag && start<ans.COIN_COUNT ; ++start)
+    {
+        tmpcurr = curr;
+        Money tmpans = ans;
+        for (int i = start ; (i%ans.COIN_COUNT)!=start || !startflag ; ++i)
+        {
+            startflag = true;
+            i%=ans.COIN_COUNT;
+            const int& val = tmpans.CoinValue[i];
+            int& qty = tmpans[i];
+            int newqty = tmpcurr/val;
+            if (newqty>qty)
+            {
+                newqty = qty;
+                qty = 0;
+            }
+            else
+            {
+                qty-=newqty;
+            }
+            tmpcurr-= (newqty*val);
+            flag = tmpcurr;
 
-	//TODO: BELAL
+        }
+        startflag =0;
+        if (!flag)
+        {
+            //To print the change if needed
+           std :: cout << "Here is your change : "<<std :: endl;
+            for (int i = 0 ; i<ans.COIN_COUNT ; ++i)
+            {
+                int val = ans.CoinValue[i] - tmpans.CoinValue[i];
+                int q = ans[i] - tmpans[i];
+                std :: cout << val << " " << q << std :: endl;
+            }
+            ans = tmpans;
+            return 1;
+        }
+    }
 	return 0;
 }
 BuyResult VendingMachine::buyItem(int idx, const Money& src, Money& rem)

a4240174184f0ebd1f454c9ef3e703a4993e4dfb
Belal Hamdy <36121094+belalhamdy@users.noreply.github.com>
2018-11-17 21:55:33 +0200

    added comments to elaborate.

--- Assignment 3/Task 1/include/Money.h
+++ Assignment 3/Task 1/include/Money.h
@@ -2,7 +2,7 @@
 #define _MONEY_H
 #include <string>
 #include <iostream>
-
+// enum to access the array by the name of currency
 enum CoinIndex {
 	QUARTERS = 0,
 	HALFS,
@@ -17,14 +17,14 @@ enum CoinIndex {
 class Money
 {
 public:
-	const static int COIN_COUNT = 9;
-	const static int CoinValue[COIN_COUNT];
-	const static std::string CoinNames[COIN_COUNT];
-	const static std::string CoinNamesSingular[COIN_COUNT];
+	const static int COIN_COUNT = 9; // maximum number of coins in the machine
+	const static int CoinValue[COIN_COUNT]; // array that holds the currency values
+	const static std::string CoinNames[COIN_COUNT]; // holds the names of the currency in plural
+	const static std::string CoinNamesSingular[COIN_COUNT]; // also holds the names of currency but in single
 
-	Money(int Quantities[COIN_COUNT]);
-	Money(int Quantity = 0);
-	Money(const Money& other);
+	Money(int Quantities[COIN_COUNT]); // if the quantities of currency are different
+	Money(int Quantity = 0); // if the quantities of currency are same
+	Money(const Money& other); // copy constructor for this class
 
 	int getTotalValue() const;
 
@@ -38,7 +38,7 @@ public:
 
 	Money& operator =(const Money& other);
 
-	friend std::ostream& operator << (std::ostream& out, const Money& mon);
+	friend std::ostream& operator << (std::ostream& out, const Money& mon); // operator to print the quantities
 protected:
 	int quantity[COIN_COUNT];
 private:

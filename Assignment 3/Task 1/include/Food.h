38fb5b6549edb145c083f5488049ff194cc5febd
Belal Hamdy <36121094+belalhamdy@users.noreply.github.com>
2018-11-17 21:55:05 +0200

    added comments to elaborate.

--- Assignment 3/Task 1/include/Food.h
+++ Assignment 3/Task 1/include/Food.h
@@ -6,12 +6,12 @@ class Food
 {
 public:
 	Food();
-	Food(std::string n, int priceInPiastres, int q = 10);
+	Food(std::string n, int priceInPiastres, int q = 10); // takes the name of the item and the price and the quantity (default 10 as required)
 	Food(std::string n, double priceInPounds, int q = 10);
 	int getPrice() const;
 	std::string getName() const;
 	int getQuantity() const;
-	void takeOne();
+	void takeOne(); // buying one item of this item if it available
 protected:
 	int price;
 	std::string name;

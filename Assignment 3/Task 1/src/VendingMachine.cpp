1aba2c390b94dc01ff6933ac68a951b934bc82aa
Belal Hamdy <36121094+belalhamdy@users.noreply.github.com>
2018-11-17 21:53:59 +0200

    added comments to elaborate.

--- Assignment 3/Task 1/src/VendingMachine.cpp
+++ Assignment 3/Task 1/src/VendingMachine.cpp
@@ -4,6 +4,7 @@ VendingMachine::VendingMachine()
 {
 
 }
+// copying the given items and coins to the machine
 VendingMachine::VendingMachine(Food* items, const Money& initialCoins)
 {
 	for (int i = 0; i < MAX_ITEMS; ++i)
@@ -11,6 +12,7 @@ VendingMachine::VendingMachine(Food* items, const Money& initialCoins)
 
 	coins = initialCoins;
 }
+// calculating the remainder of the given money by backtracking and trying all possible cases
 bool VendingMachine::convertMoneyRecurse(int qty, int taken, int i, Money& ans) const
 {
 	if (i == -1) return qty == 0;
@@ -30,12 +32,13 @@ bool VendingMachine::convertMoneyRecurse(int qty, int taken, int i, Money& ans) 
 	}
 	return 0;
 }
-
+// calls the recursive function but with different parameters
 bool VendingMachine::convertMoney(int curr, Money& ans) const
 {
 	ans = Money(0);
 	return convertMoneyRecurse(curr, 0, Money::COIN_COUNT - 1, ans);
 }
+// checking the item is valid to buy (index , available quantity) , then gives the machine the money and returns the change
 BuyResult VendingMachine::buyItem(int idx, const Money& src, Money& rem)
 {
 	if (idx < 0 || idx >= MAX_ITEMS)
